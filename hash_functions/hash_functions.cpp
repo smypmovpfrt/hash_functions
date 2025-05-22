47a00a48296552e993e589f7dabb55bc72ddce11
Anton <bcp.anton@gmail.com>
2025-05-22 18:10:04 +0300

    start

--- hash_functions/hash_functions.cpp
+++ hash_functions/hash_functions.cpp
@@ -3,21 +3,5 @@
 
 
 int main() {
-	std::map<std::string, unsigned> products {
-		{"bread", 30}, {"milk", 80}, {"apple", 60}
-	};
-
-	//до 17 стандарта (value.first (key), value.second (value)
-
-	for (const auto& value : products) {
-		std::cout << value.second << std::endl;
-	}
-	
-	//работает начиная с 17 стандарта:
-
-	for (const auto& [pKey, pValue] : products) {
-		std::cout << pKey << '\t' << pValue << std::endl;
-	}
-
 
 }
