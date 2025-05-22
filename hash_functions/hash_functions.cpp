a8f2267dd316fe45978aa18b800021fbb710b23f
Anton <bcp.anton@gmail.com>
2025-05-22 18:54:36 +0300

    BetterIdea

--- hash_functions/hash_functions.cpp
+++ hash_functions/hash_functions.cpp
@@ -2,8 +2,22 @@
 #include <iostream>
 #include <array>
 
+int GetIndex(int n) {
+	if (n == 10)	return 0;
+	if (n == 50)	return 1;
+	if (n == 100)	return 2;
+	if (n == 500)	return 3;
+	if (n == 1000)	return 4;
+	if (n == 2000)	return 5;
+	if (n == 5000)	return 6;
+}
 
-int main() {
+
+void BadIdea() {
+
+	//Плохой способ. Так как для маленького количества купюр выделяется большой вектор. 
+	//Купюра может быть и с огромным номиналом (Африка), в векторе просто не хватит индексов...
+
 	int nominal;
 	std::vector<int> cash(5001);
 	while (std::cin >> nominal) {
@@ -15,4 +29,25 @@ int main() {
 			std::cout << i << " - " << cash[i] << std::endl;
 		}
 	}
+}
+
+void BetterIdea() {
+	int nominal;
+	std::vector<int> cash(8);
+	while (std::cin >> nominal) {
+		cash[GetIndex(nominal)]++;
+	}
+
+	static std::array<int, 8> names = { 10, 50, 100, 200, 500, 1000, 2000, 5000 };
+
+
+	for (int i = 0; i < cash.size(); i++) {
+		if (cash[i] != 0) {
+			std::cout << names[i] << " - " << cash[i] << std::endl;
+		}
+	}
+}
+
+int main() {
+	BetterIdea();
 }
