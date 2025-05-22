b194dcdcf0cbd50bc298d4ed8a8d70ab7249cb8e
Anton <bcp.anton@gmail.com>
2025-05-22 18:28:10 +0300

    works

--- hash_functions/hash_functions.cpp
+++ hash_functions/hash_functions.cpp
@@ -1,7 +1,18 @@
+#include <vector>
 #include <iostream>
-#include <map>
+#include <array>
 
 
 int main() {
+	int nominal;
+	std::vector<int> cash(5001);
+	while (std::cin >> nominal) {
+		cash[nominal]++;
+	}
 
+	for (int i = 0; i < cash.size(); i++) {
+		if (cash[i] != 0) {
+			std::cout << i << " - " << cash[i] << std::endl;
+		}
+	}
 }
